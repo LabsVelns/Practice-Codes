b93e373b7d923187b5e3ff9d80aa0e130465116d
LabsVelns <harshadmane1293@gmail.com>
2026-06-06 15:24:58 +0530

    Added pydantic implementation

--- Fastapi.py
+++ Fastapi.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import uvicorn as uv
+from Pydantic_classes import User
 
 app = FastAPI(title="FastAPI-with-Swagger/Openapi")
 
@@ -10,6 +11,16 @@ def index():
 @app.get('/Welcome')
 def get_name(name : str):
     return {f"Hi {name} it is very nice to meet you."}
+
+@app.post('/Welcome/Details')
+def get_details(Details:User):
+    Details = Details.model_dump()
+    Name = Details["Name"]
+    Age = Details["Age"]
+    Salary = Details["Salary"]
+
+    return {f"Hi {Name} it is very nice to meet you. Your age is {Age} and your salary is {Salary}"}
+
 
 
 if __name__ == '__main__':
